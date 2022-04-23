796edce763e24c5ead1edbf4f21f16ac9ba2feba
artur <shadaraevd@gmail.com>
2022-04-23 14:28:47 +0800

    add args

--- page_loader/parse_args.py
+++ page_loader/parse_args.py
@@ -2,11 +2,19 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        usage="Usage page-loader [options] <url>",
+        description="some description",
+    )
     parser.add_argument(metavar='url', type=str, dest='url')
+    parser.add_argument('-V', '--version', help='output the version number',
+                        action='version', version='0.1.0')
     parser.add_argument('-o', '--output', dest='output', type=str,
                         metavar='output', help='output dir (default: "/app")',
                         default='/app')
-
+    parser.add_argument('-h', '--help', help='display help for command',
+                        action='help')
+    parser._optionals.title = 'Options'
     args = parser.parse_args()
     return args
