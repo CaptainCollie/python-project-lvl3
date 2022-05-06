30a667e2df84f3926756fc760dc818ae283773fa
artur <shadaraevd@gmail.com>
2022-05-06 20:26:17 +0800

    refactor

--- page_loader/utils.py
+++ page_loader/utils.py
@@ -3,18 +3,22 @@ import re
 from urllib.parse import urlparse
 
 
-def transform_url_to_file_name(url: str, postfix: str,
+def transform_url_to_file_name(url: str, postfix: str = '',
                                is_dir: bool = False) -> str:
     """Transforms url to file name
     https://ru.hexlet.io/courses -> ru-hexlet-io-courses.html"""
     parsed_url = urlparse(url)
+    default_postfix = ''
     if len(parsed_url.path.split('.')) == 2:
-        if not postfix:
-            postfix = parsed_url.path.split('.')[-1]
+        default_postfix = parsed_url.path.split('.')[-1]
         parsed_url = parsed_url._replace(
             path=''.join(parsed_url.path.split('.')[:-1]))
-    elif not postfix:
+
+    if not postfix and not default_postfix:
         postfix = 'html'
+    elif not postfix:
+        postfix = default_postfix
+
     if not is_dir:
         postfix = '.' + postfix
 
